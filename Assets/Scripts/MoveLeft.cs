a6c3c6e96c52cc6d31aa398a820225cbd2d2af49
celerywastaken <152607041+celerywastaken@users.noreply.github.com>
2023-12-19 15:46:19 +0100

    Lab 4.2

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,19 +6,25 @@ public class MoveLeft : MonoBehaviour
 {
     public float speed = 30.0f;
     public float leftBOund = -5;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * -1 * Time.deltaTime * speed); 
-        if (transform.position.x  < leftBOund)
+        //transform.Translate(Vector3.right * -1 * Time.deltaTime * speed); 
+        if (transform.position.x  < leftBOund && gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
         }
+        if (playerControllerScript.GameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed); // putting the movement into an if statement, so if we're game iver it doesnt move
+        }
     }
+
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,13 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle",startDelay,repeatRate);
        // Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+       playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,11 @@ public class SpawnManager : MonoBehaviour
     }
     private void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.GameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            //the same thing as for moving, make the spawning dependant on gameover being false.
+        }
+        
     }
 }
